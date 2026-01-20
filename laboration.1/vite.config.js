d20a8d07d4d6e0054c27ee5f673f0f1552304e80
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-01-20 18:05:31 +0100

    Lagt till html-sidorna för projektet

--- laboration.1/vite.config.js
+++ laboration.1/vite.config.js
@@ -2,14 +2,15 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 
-export default defineConfig(
+export default defineConfig({
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
                 pictures: resolve(__dirname, "pictures.html"),
-                process: resolve(__dirname, "process.html")
+                process: resolve(__dirname, "process.html"),
             }
         }
     }
+}
 );
