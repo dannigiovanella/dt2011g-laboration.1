451b5593ad7d6c0ed8a12faafbfb3beeabefddf2
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-01-20 19:04:52 +0100

    Lagt in plug in för att optimera bilder med vite

--- laboration.1/vite.config.js
+++ laboration.1/vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import ViteImageOptimizer from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "index.html"),
-                pictures: resolve(__dirname, "pictures.html"),
-                process: resolve(__dirname, "process.html"),
-            }
-        }
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        pictures: resolve(__dirname, "pictures.html"),
+        process: resolve(__dirname, "process.html"),
+      }
     }
-}
-);
+  },
+  plugins: [
+    ViteImageOptimizer({
+      
+
+        
+    })
+  ]
+});
