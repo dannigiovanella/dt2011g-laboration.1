b51b845398a1fd057f352364e7832d955ab3cb88
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-01-25 14:46:04 +0100

    Fixat småbuggar i navmeny

--- laboration.1/vite.config.js
+++ laboration.1/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import ViteImageOptimizer from 'vite-plugin-image-optimizer';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   build: {
